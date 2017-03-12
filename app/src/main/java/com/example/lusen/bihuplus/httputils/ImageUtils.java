fb6d61798d3dac430c43e95449f0f9518e2650a4
lusen <836471223@qq.com>
2017-03-12 16:34:16 +0800

    命名修改

--- app/src/main/java/com/example/lusen/bihuplus/httputils/ImageUtils.java
+++ app/src/main/java/com/example/lusen/bihuplus/httputils/ImageUtils.java
@@ -1,13 +1,7 @@
 package com.example.lusen.bihuplus.httputils;
 
-import android.accounts.NetworkErrorException;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Handler;
-
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 /**
  * Created by lusen on 2017/2/9.
@@ -34,40 +28,3 @@ public class ImageUtils {
     }
 }
 
-class ImageNet{
-    public static Bitmap get(final String url) {
-        HttpURLConnection conn = null;
-        Bitmap bmp = null;
-        try {
-            // 利用string url构建URL对象
-            URL mURL = new URL(url);
-            conn = (HttpURLConnection) mURL.openConnection();
-//            conn.setRequestMethod("GET");
-            conn.setReadTimeout(5000);
-            conn.setDoInput(true);
-            conn.setUseCaches(false);
-            conn.setConnectTimeout(10000);
-
-            int responseCode = conn.getResponseCode();
-            if (responseCode == 200) {
-                conn.connect();
-                InputStream is = conn.getInputStream();
-                bmp = BitmapFactory.decodeStream(is);
-                is.close();
-
-            } else {
-                throw new NetworkErrorException("response status is "+responseCode);
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-
-            if (conn != null) {
-                conn.disconnect();
-            }
-        }
-        return bmp;
-    }
-
-}
